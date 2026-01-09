137ecde4bc298c724db023c5204399966573193c
Niklas Melin <niklasme@pm.me>
2026-01-09 23:08:07 +0100

    dev(SolarEdgeModbus): Fix coding standard and pass black test

--- src/SolarEdgeInverter.py
+++ src/SolarEdgeInverter.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 import solaredge_modbus
 
@@ -5,7 +6,7 @@ import solaredge_modbus
 logging.basicConfig(
     level=logging.INFO,  # Set the default logging level
     format="%(asctime)s - %(levelname)s - %(name)s - %(funcName)s - %(message)s",  # Log format
-    datefmt="%Y-%m-%d %H:%M:%S"  # Date format
+    datefmt="%Y-%m-%d %H:%M:%S",  # Date format
 )
 
 # Create a logger instance
@@ -31,71 +32,93 @@ REGISTER_SCALE_FACTORS = {
     "current_dc": "current_dc_scale",
     "voltage_dc": "voltage_dc_scale",
     "power_dc": "power_dc_scale",
-    "temperature": "temperature_scale"
+    "temperature": "temperature_scale",
 }
 
-SUNSPEC_REGISTERS = ["c_id",
-                     "c_did",
-                     "c_length",
-                     "c_manufacturer",
-                     "c_model",
-                     "c_version",
-                     "c_serialnumber",
-                     "c_deviceaddress",
-                     "c_sunspec_did",
-                     "c_sunspec_length"]
+SUNSPEC_REGISTERS = [
+    "c_id",
+    "c_did",
+    "c_length",
+    "c_manufacturer",
+    "c_model",
+    "c_version",
+    "c_serialnumber",
+    "c_deviceaddress",
+    "c_sunspec_did",
+    "c_sunspec_length",
+]
 
-INVERTER_STATUS_REGISTERS = ["status",
-                             "vendor_status",
-                             "rrcr_state",
-                             "active_power_limit",
-                             "cosphi",
-                             "commit_power_control_settings",
-                             "restore_power_control_default_settings",
-                             "reactive_power_config",
-                             "reactive_power_response_time",
-                             "advanced_power_control_enable",
-                             "export_control_mode",
-                             "export_control_limit_mode",
-                             "export_control_site_limit",
-                             "storage_control_mode",
-                             "storage_ac_charge_policy",
-                             "storage_ac_charge_limit",
-                             "storage_backup_reserved_setting",
-                             "storage_default_mode",
-                             "rc_cmd_timeout",
-                             "rc_cmd_mode",
-                             "rc_charge_limit",
-                             "rc_discharge_limit"]
+INVERTER_STATUS_REGISTERS = [
+    "status",
+    "vendor_status",
+    "rrcr_state",
+    "active_power_limit",
+    "cosphi",
+    "commit_power_control_settings",
+    "restore_power_control_default_settings",
+    "reactive_power_config",
+    "reactive_power_response_time",
+    "advanced_power_control_enable",
+    "export_control_mode",
+    "export_control_limit_mode",
+    "export_control_site_limit",
+    "storage_control_mode",
+    "storage_ac_charge_policy",
+    "storage_ac_charge_limit",
+    "storage_backup_reserved_setting",
+    "storage_default_mode",
+    "rc_cmd_timeout",
+    "rc_cmd_mode",
+    "rc_charge_limit",
+    "rc_discharge_limit",
+]
 
-INVERTER_POLL_REGISTERS = ["current",                        # 40071 - A  - AC Total Current
-                           "l1_current",                     # 40072 - A  - AC Line 1 Current
-                           "l2_current",                     # 40073 - A  - AC Line 2 Current
-                           "l3_current",                     # 40074 - A  - AC Line 3 Current
-                           "l1n_voltage",                    # 40080 - A  - AC Line 1 to Neutral Voltage
-                           "l2n_voltage",                    # 40081 - A  - AC Line 2 to Neutral Voltage
-                           "l3n_voltage",                    # 40082 - A  - AC Line 3 to Neutral Voltage
-                           "power_ac",                       # 40084 - A  - AC Active Power
-                           "frequency",                      # 40071 - Hz - AC Frequency
-                           "energy_total",                   # 40071 - Wh - AC Lifetime Energy production
-                           "current_dc",                     # 40071 - A  - DC Current
-                           "voltage_dc",                     # 40071 - V  - DC Voltage
-                           "temperature",                    # 40104 - C  - Heat Sink Temperature
+INVERTER_POLL_REGISTERS = [
+    "current",  # 40071 - A  - AC Total Current
+    "l1_current",  # 40072 - A  - AC Line 1 Current
+    "l2_current",  # 40073 - A  - AC Line 2 Current
+    "l3_current",  # 40074 - A  - AC Line 3 Current
+    "l1n_voltage",  # 40080 - A  - AC Line 1 to Neutral Voltage
+    "l2n_voltage",  # 40081 - A  - AC Line 2 to Neutral Voltage
+    "l3n_voltage",  # 40082 - A  - AC Line 3 to Neutral Voltage
+    "power_ac",  # 40084 - A  - AC Active Power
+    "frequency",  # 40071 - Hz - AC Frequency
+    "energy_total",  # 40071 - Wh - AC Lifetime Energy production
+    "current_dc",  # 40071 - A  - DC Current
+    "voltage_dc",  # 40071 - V  - DC Voltage
+    "temperature",  # 40104 - C  - Heat Sink Temperature
+    "current_scale",  # 40076 - Scale factor for Current values
+    "energy_total_scale",  # 40079 - Scale factor for Energy values
+    "voltage_scale",  # 40083 - Scale factor for Voltage values
+    "power_ac_scale",  # 40085 - Scale factor for AC Power values
+    "power_dc_scale",  # 40102 - Scale factor for DC Power values
+    "temperature_scale",  # 40105 - Scale factor for Temperature values
+    "status",  # 40108 - Inverter Status
+    "vendor_status",  # 40109 - Vendor Specific Status Code and error codes
+    "active_power_limit",  # 61441 - F001 - Percent of maximum power (1-100)
+    "commit_power_control_settings",  # 61696 - F100 - Commit Power Control Settings
+    "restore_power_control_default_settings",  # 61761 - F101 - Restore Power Control Default Settings
+]
 
-                           "current_scale",                  # 40076 - Scale factor for Current values
-                           "energy_total_scale",             # 40079 - Scale factor for Energy values
-                           "voltage_scale",                  # 40083 - Scale factor for Voltage values
-                           "power_ac_scale",                 # 40085 - Scale factor for AC Power values
-                           "power_dc_scale",                 # 40102 - Scale factor for DC Power values
-                           "temperature_scale",              # 40105 - Scale factor for Temperature values
-
-                           "status",                         # 40108 - Inverter Status
-                           "vendor_status",                  # 40109 - Vendor Specific Status Code and error codes
-
-                           "active_power_limit",             # 61441 - F001 - Percent of maximum power (1-100)
-                           "commit_power_control_settings",  # 61696 - F100 - Commit Power Control Settings
-                           "restore_power_control_default_settings",  # 61761 - F101 - Restore Power Control Default Settings
-                           ]
+INVERTER_CONTROL_REGISTERS = [
+    "current",  # 40071 - A  - AC Total Current
+    "l1_current",  # 40072 - A  - AC Line 1 Current
+    "l2_current",  # 40073 - A  - AC Line 2 Current
+    "l3_current",  # 40074 - A  - AC Line 3 Current
+    "power_ac",  # 40084 - A  - AC Active Power
+    "energy_total",  # 40071 - Wh - AC Lifetime Energy production
+    "current_dc",  # 40071 - A  - DC Current
+    "voltage_dc",  # 40071 - V  - DC Voltage
+    "current_scale",  # 40076 - Scale factor for Current values
+    "energy_total_scale",  # 40079 - Scale factor for Energy values
+    "voltage_scale",  # 40083 - Scale factor for Voltage values
+    "power_ac_scale",  # 40085 - Scale factor for AC Power values
+    "power_dc_scale",  # 40102 - Scale factor for DC Power values
+    "status",  # 40108 - Inverter Status
+    "vendor_status",  # 40109 - Vendor Specific Status Code and error codes
+    "active_power_limit",  # 61441 - F001 - Percent of maximum power (1-100)
+    "restore_power_control_default_settings",  # 61761 - F101 - Restore Power Control Default Settings
+]
 
 
 class SolarEdgeInverter(solaredge_modbus.Inverter):
@@ -104,6 +127,10 @@ class SolarEdgeInverter(solaredge_modbus.Inverter):
         Initializes the SolarEdgeInverter class with default values.
         :param device: The device path for the inverter connection (default: "/dev/ttyUSB0").
         :param baud: The baud rate for the inverter connection (default: 9600).
+        :param timeout: The timeout for the inverter connection in seconds (default: 2).
+
+        Typically connects via Modbus RTU over a serial interface and then reads all registers, using the get_inverter_data method to populate the class attributes.
+        The class provides methods to read and print inverter data, as well as set production limits.
         """
 
         # Setup logging
@@ -163,17 +190,24 @@ class SolarEdgeInverter(solaredge_modbus.Inverter):
         self.export_control_limit_mode: int = None
         self.export_control_site_limit: float = None
 
-        # Read inverter data
-        self._read_inverter_all()
+    async def check_connection(self) -> bool:
+        """
+        Checks the connection to the inverter.
+        :return: True if connected, False otherwise.
+        """
+        self.connect()
+        is_connected = self.connected()
+        self.disconnect()
+        return is_connected
 
-    def _read_inverter_all(self):
+    async def get_inverter_data(self):
         """
         Connects to the inverter, reads all registers, applies scaling factors,
         and stores the scaled data in the class attributes.
         """
         # Initialize the connection
         super().__init__(device=self.device, baud=self.baud, timeout=self.timeout)
-        self.connect()
+        await self.connect()
 
         if not self.connected():
             logger.error("Failed to connect to the inverter.")
@@ -197,7 +231,10 @@ class SolarEdgeInverter(solaredge_modbus.Inverter):
         logger.debug("Read scaling factors and setting registers...")
         for key, scale_key in REGISTER_SCALE_FACTORS.items():
             if key in registers and scale_key in registers:
-                scaled_value = round(float(registers[key]) * (10 ** int(registers[scale_key])), int(registers[scale_key]))
+                scaled_value = round(
+                    float(registers[key]) * (10 ** int(registers[scale_key])),
+                    int(registers[scale_key]),
+                )
 
             # Store the scaled value in the corresponding attribute
             if hasattr(self, key):
@@ -219,10 +256,9 @@ class SolarEdgeInverter(solaredge_modbus.Inverter):
                     logger.warning(f"Warning: Attribute {key} not found in class.")
 
         # Disconnect after reading
-        self.disconnect()
+        await self.disconnect()
 
-    def read_inverter_poll_registers(self):
-        
+    async def update_cashed_poll_inverter_registers(self):
         """
         Connects to the inverter, reads the poll registers, applies scaling factors,
         and stores the scaled data in the class attributes.
@@ -244,14 +280,18 @@ class SolarEdgeInverter(solaredge_modbus.Inverter):
             except Exception as e:
                 logger.error(f"Error reading register {register}: {e}")
                 read_registers[register] = None
-        
-        print(read_registers)
+
+        # Disconnect after reading
+        self.disconnect()
 
         # Apply scaling factors and store the scaled data
         logger.debug("Read scaling factors and setting registers...")
         for key, scale_key in REGISTER_SCALE_FACTORS.items():
             if key in read_registers and scale_key in read_registers:
-                scaled_value = round(float(read_registers[key]) * (10 ** int(read_registers[scale_key])), int(read_registers[scale_key]))
+                scaled_value = round(
+                    float(read_registers[key]) * (10 ** int(read_registers[scale_key])),
+                    int(read_registers[scale_key]),
+                )
 
             # Store the scaled value in the corresponding attribute
             if hasattr(self, key):
@@ -260,39 +300,76 @@ class SolarEdgeInverter(solaredge_modbus.Inverter):
             else:
                 logger.warning(f"Warning: Attribute {key} not found in class.")
 
+    async def update_cashed_control_inverter_registers(self):
+        """
+        Connects to the inverter, reads the poll registers, applies scaling factors,
+        and stores the scaled data in the class attributes.
+        """
+        # Read poll registers
+        logger.info("Reading inverter poll registers from the inverter...")
+
+        # Initialize the connection
+        self.connect()
+
+        if not self.connected():
+            logger.error("Failed to connect to the inverter.")
+            raise ConnectionError("Failed to connect to the inverter.")
+
+        read_registers = {}
+        for register in INVERTER_CONTROL_REGISTERS:
+            try:
+                read_registers.update(self.read(register))
+            except Exception as e:
+                logger.error(f"Error reading register {register}: {e}")
+                read_registers[register] = None
+
         # Disconnect after reading
         self.disconnect()
-    
-    def get_inverter_registers_as_json(self):
+
+        # Apply scaling factors and store the scaled data
+        logger.debug("Read scaling factors and setting registers...")
+        for key, scale_key in REGISTER_SCALE_FACTORS.items():
+            if key in read_registers and scale_key in read_registers:
+                scaled_value = round(
+                    float(read_registers[key]) * (10 ** int(read_registers[scale_key])),
+                    int(read_registers[scale_key]),
+                )
+
+            # Store the scaled value in the corresponding attribute
+            if hasattr(self, key):
+                setattr(self, key, scaled_value)
+                logger.debug(f"\t{key:<40s} - {scaled_value}")
+            else:
+                logger.warning(f"Warning: Attribute {key} not found in class.")
+
+    def get_cashed_inverter_registers_as_json(self):
         """
         Returns the current status of registers as a JSON-like dictionary.
         """
         logger.info("Get inverter registers as json")
 
-        # self.read_inverter_poll_registers()
-        
         all_data = {}
         for attribute in dir(self):
-            if attribute in SUNSPEC_REGISTERS + INVERTER_POLL_REGISTERS and not callable(getattr(self, attribute)):
+            if (attribute in SUNSPEC_REGISTERS + INVERTER_POLL_REGISTERS and not callable(getattr(self, attribute))):
                 value = getattr(self, attribute)
                 all_data[attribute] = value
-                
+
         return all_data
-    
-    def get_inverter_poll_registers_as_json(self):
+
+    def get_cashed_data_as_json(self):
         """
         Returns the current status of all poll registers as a JSON-like dictionary.
         """
         logger.info("Get inverter poll registers as json")
 
-        #self.read_inverter_poll_registers()
-        
         poll_data = {}
         for attribute in dir(self):
-            if attribute in INVERTER_POLL_REGISTERS and not callable(getattr(self, attribute)):
+            if attribute in INVERTER_POLL_REGISTERS and not callable(
+                getattr(self, attribute)
+            ):
                 value = getattr(self, attribute)
                 poll_data[attribute] = value
-                
+
         return poll_data
 
     def print_inverter_data(self):
@@ -311,14 +388,18 @@ class SolarEdgeInverter(solaredge_modbus.Inverter):
         """
         Prints the current status of all registers.
         """
-        print("\nInverter Registers and settings: \n\tRegister and settings                   : Value")
+        print(
+            "\nInverter Registers and settings: \n\tRegister and settings                   : Value"
+        )
         for attribute in dir(self):
-            if not attribute.startswith("_") and not callable(getattr(self, attribute)) and not attribute in ["registers", "battery_dids", "client", "meter_dids", "logger"]:
+            if (
+                not attribute.startswith("_") and not callable(getattr(self, attribute)) and attribute not in ["registers", "battery_dids", "client", "meter_dids", "logger"]
+            ):
                 value = getattr(self, attribute)
                 print(f"\t{attribute:<40}: {value}")
         print("\n")
 
-    def set_production_limit(self, limit: int):
+    async def set_production_limit(self, limit: int):
         """
         Sets the production limit of the inverter. These settings are volatile and will be lost on power cycle.
 
@@ -360,7 +441,7 @@ class SolarEdgeInverter(solaredge_modbus.Inverter):
         # Disconnect after setting
         self.disconnect()
 
-    def set_restore_power_control_defaults(self):
+    async def set_restore_power_control_defaults(self):
         """
         Restores the power control settings to their default values.
 
@@ -380,14 +461,13 @@ class SolarEdgeInverter(solaredge_modbus.Inverter):
         logger.info("Restoring power control default settings...")
         self.write("restore_power_control_default_settings", 1)
 
-
-       # Set active power limit
+        # Set active power limit
         logger.info("Revert setting active power limit to 100%")
         self.write("active_power_limit", 100)
-        
+
         # Apply the setting
         # logger.info("Committing power control settings...")
         # self.write("commit_power_control_settings", 1)
-        
+
         # Disconnect after setting
         self.disconnect()
